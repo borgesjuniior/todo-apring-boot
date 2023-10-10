10ebfaeb695206e7fbb97dc6badac9807de8c772
Junior <valdecyborgesjr@hotmail.com>
2023-10-10 15:23:24 -0300

    setting the username as unique

--- src/main/java/com/todo/user/IUserRepository.java
+++ src/main/java/com/todo/user/IUserRepository.java
@@ -5,5 +5,8 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<User, UUID> {
-
+  /*
+   * Método customizado findBy<nome do atributo>
+   */
+  User findByUsername(String username);
 }
